f08d741b06e1e4d5649051094825874446b1cedb
Michaeltftaylor <129795804+Michaeltftaylor@users.noreply.github.com>
2023-06-27 21:16:52 -0300

    commit now

---
+++ index.js
@@ -0,0 +1,36 @@
+// Função para atualizar o relógio
+function atualizarRelogio() {
+    var dataAtual = new Date();
+    
+    var horas = formatarNumero(dataAtual.getHours());
+    var minutos = formatarNumero(dataAtual.getMinutes());
+    var segundos = formatarNumero(dataAtual.getSeconds());
+
+    var diaSemana = obterDiaSemana(dataAtual.getDay());
+    var dia = formatarNumero(dataAtual.getDate());
+    var mes = formatarNumero(dataAtual.getMonth() + 1); // +1 porque os meses são indexados de 0 a 11
+    var ano = dataAtual.getFullYear();
+    
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
+
+    document.getElementById("diaData").textContent = `${diaSemana}, ${dia}/${mes}/${ano}`;
+  }
+  
+  // Função para formatar números menores que 10 adicionando um zero à esquerda
+  function formatarNumero(numero) {
+    if (numero < 10) {
+      return "0" + numero;
+    }
+    return numero;
+  }
+
+  // Função para obter o nome do dia da semana
+function obterDiaSemana(dia) {
+  var diasSemana = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return diasSemana[dia];
+}
+
+// Chamar a função para atualizar o relógio e a data a cada segundo
+  setInterval(atualizarRelogio, 1000);
